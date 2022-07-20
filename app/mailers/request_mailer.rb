b9b441c89871f0bc63fc2e52b9d162e050f0d711
riki0808 <sr28329732@gmail.com>
2022-07-20 14:42:33 +0900

    ActionMailerのメールにファイルを添付させた

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -3,7 +3,9 @@ class RequestMailer < ApplicationMailer
   def request_email
 
     @doc = Doc.find(params[:doc])
-    attachments[@doc.main.file.basename] = File.read(".#{File.dirname(@doc.main.url)}/#{@doc.main.file.basename}.pdf")
+    attachments["#{@doc.main.file.basename}.pdf"] = { 
+      :content => File.read("public/#{File.dirname(@doc.main.url)}/#{@doc.main.file.basename}.pdf")
+    }
     mail(
       subject: 'slideboxの依頼がきたよっ',
       from: Rails.application.credentials.gmail[:user_name],
