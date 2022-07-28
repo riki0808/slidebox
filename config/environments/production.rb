0928b1edf5ce117b6d48f73abce826b017c706ab
riki0808 <sr28329732@gmail.com>
2022-07-28 16:35:37 +0900

    production.rbに決め打ちでhostを設定した

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "slidebox_production"
 
+  config.action_mailer.default_url_options = { host: 'sllidebox.herokuapp.com'}
   config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
