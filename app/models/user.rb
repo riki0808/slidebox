351cfa7d65a28aef456397c3bfbf928f088b2699
riki0808 <sr28329732@gmail.com>
2022-07-28 10:27:37 +0900

    userのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   # VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{8,32}\z/i.freeze
   # validates :password, format: { with: VALID_PASSWORD_REGEX, massege: 'は半角英数を両方含む必要があります' }, on: :create
   # validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
-  # validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  # validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   has_one :setting
   has_many :docs
